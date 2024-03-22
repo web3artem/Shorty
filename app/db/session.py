fbecf59ca49a666609238bb88666d1e3a8e9a28e
cringecode <tyoma.dar@gmail.com>
2024-03-22 23:29:48 +0300

    Created async engine and session for interaction with DB

---
+++ app/db/session.py
@@ -0,0 +1,24 @@
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.config import settings
+
+
+def new_async_engine(uri: URL) -> AsyncEngine:
+    return create_async_engine(uri, future=True, echo=True)
+
+
+# create async engine for interaction with db
+_ASYNC_ENGINE = new_async_engine(settings.DATABASE_URL)
+
+# create session for interacting with db
+_ASYNC_SESSIONMAKER = async_sessionmaker(_ASYNC_ENGINE, expire_on_commit=False)
+
+
+def get_async_session() -> AsyncSession:
+    return _ASYNC_SESSIONMAKER()
