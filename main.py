6cc5cf8aa49ffb4ae57c5b8b5ecd43fd825830d4
cringecode <tyoma.dar@gmail.com>
2024-04-04 22:37:23 +0300

    small fixes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from fastapi.responses import RedirectResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.handlers import url_router
+from app.db.session import get_db
+from app.services.url import get_full_url_by_short
+
 
 app = FastAPI()
 
@@ -11,20 +16,16 @@ main_app_router.include_router(url_router, prefix="/url", tags=["url"])
 app.include_router(main_app_router)
 
 
-# async def get_db():
-#     async with get_async_session() as session:
-#         new_url = ShortUrlORM(
-#             full_url="https://www.perplexity.ai/search/python-JU9gGWCnRHdu_umso2QX9GQ",
-#             short_url="assdo",
-#         )
-#         session.add(new_url)
-#         await session.flush()
-#         await session.commit()
-
-
 @app.get("/ping")
 async def ping():
     return {"ping": "pong"}
 
-if __name__ == '__main__':
+
+@app.get("/{short_url}")
+async def redirect_to_url(short_url: str, db: AsyncSession = Depends(get_db)):
+    full_url = await get_full_url_by_short(short_url, db)
+    return RedirectResponse(full_url)
+
+
+if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8001)
