fa808cd053bc97721a50921411dcc9c9e072d6fb
cringecode <tyoma.dar@gmail.com>
2024-04-04 22:37:00 +0300

    Now funcs getting a db dependency instead of async_session

--- app/services/url.py
+++ app/services/url.py
@@ -9,35 +9,33 @@ from app.schemas.url import URLItem, URLModelFromDB
 
 
 # Creating a randomized shorten url and checking it in DB
-async def shorten_url(session_factory: AsyncSession):
-    async with session_factory as session:
-        digits_and_letters = digits + ascii_letters
-        short_url = "".join([random.choice(digits_and_letters) for _ in range(7)])
-
-        query = (
-            select(ShortUrlORM)
-            .where(ShortUrlORM.short_url == short_url)
-        )
-        result = await session.execute(query)
-
-        if result.scalars().one_or_none() is None:
-            return short_url
-        else:
-            await shorten_url(session)
+async def shorten_url(session_factory):
+    digits_and_letters = digits + ascii_letters
+    short_url = "".join([random.choice(digits_and_letters) for _ in range(7)])
+    query = select(ShortUrlORM).where(ShortUrlORM.short_url == short_url)
+    result = await session_factory.execute(query)
+    if result.scalars().one_or_none() is None:
+        return short_url
+    else:
+        await shorten_url(session_factory)
 
 
 # Get or create entity in DB
-async def get_or_create_url(body: URLItem, session: AsyncSession):
-    query = (
-        select(ShortUrlORM)
-        .where(ShortUrlORM.full_url == body.url)
-    )
-
-    result = await session.execute(query)
+async def get_or_create_url(body: URLItem, session_factory: AsyncSession):
+    query = select(ShortUrlORM).where(ShortUrlORM.full_url == body.url)
+    result = await session_factory.execute(query)
     existing_entity = result.scalars().one_or_none()
     if existing_entity is None:
-        short_url = await shorten_url(session)
-        session.add(ShortUrlORM(full_url=body.url, short_url=short_url))
-        await session.commit()
+        short_url = await shorten_url(session_factory)
+        session_factory.add(ShortUrlORM(full_url=body.url, short_url=short_url))
+        await session_factory.commit()
+        return short_url
     else:
-        return URLModelFromDB.model_validate(existing_entity)
+        return URLModelFromDB.model_validate(existing_entity).short_url
+
+
+async def get_full_url_by_short(short_url: str, session: AsyncSession):
+    query = select(ShortUrlORM.full_url).where(ShortUrlORM.short_url == short_url)
+    # print(query.compile(compile_kwargs={"literal_binds": True}))
+    result = await session.execute(query)
+    return result.scalars().first()
