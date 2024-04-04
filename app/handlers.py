a5225ed3cf929bed3b929293d903dc33ac6663a8
cringecode <tyoma.dar@gmail.com>
2024-04-04 22:37:47 +0300

    changed the name of func

--- app/handlers.py
+++ app/handlers.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.schemas.url import URLItem
 from app.services.url import get_or_create_url
-from app.db.session import get_async_session
+from app.db.session import get_db
 
 url_router = APIRouter()
 
 
 @url_router.post("/")
-async def shorten_url(body: URLItem):
-    res = await get_or_create_url(body, get_async_session())
-    print(res)
+async def create_short_url(body: URLItem, db: AsyncSession = Depends(get_db)):
+    res = await get_or_create_url(body, db)
+    return JSONResponse(
+        content={"message": f"127.0.0.1:8001/{res}"}, status_code=200
+    )
